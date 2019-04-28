0b33e9f2eaff05b15f9faeacdbb47acb6c68df4a
martynov1990 <nikimartyn@gmail.com>
2019-04-28 18:23:33 +0300

    Сделана общая для всех тестов фикстура в файле conftest.py

--- model/contact.py
+++ model/contact.py
@@ -1,7 +1,7 @@
 class Contact:
 
     def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile,
-                       work, fax, email, email2, email3, homepage, address2,
+                       work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2,
                        phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
@@ -18,6 +18,12 @@ class Contact:
         self.email2 = email2
         self.email3 = email3
         self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
         self.address2 = address2
         self.phone2 = phone2
         self.notes = notes
